4b7a59259f86ac8df014bb052b45a5f4f104c769
QarthO <QarthO@gmail.com>
2020-11-04 16:44:12 -0500

    Add files via upload

--- test files/testfile.adb
+++ test files/testfile.adb
@@ -1,13 +1,24 @@
-with Text_IO ; use Text_IO ;
+with Text_IO; use Text_IO; 
 
 procedure Main is
-   f : float := 5.99 ;
-   x : integer := 5 ;
-   y : integer := 10 ;
-   z : integer := x + y ;
-   message : string := "This is a message!" ;
+   a : integer := 5;
+   b : integer := 10;
+   c : integer := a+b;
+   message: string := "This is a message!";
+   input: String(1 .. 1);
+   
 begin
-   Put_Line ( "Hello World!" ) ;
-   Put_Line ( integer ' image ( z ) ) ;
-   Put_Line ( message ) ;
-end Main ;
+   Put_Line("Hello World!");
+   Put_Line(integer'image(c));
+   Put_Line(message);
+
+   Put_Line("1+1 = ?");
+   get(input);
+
+   if input = "2" then
+      Put_Line("Correct");
+   else
+      Put_Line("Wrong, 1+1 = 2");
+   end if;
+   
+end Main;
